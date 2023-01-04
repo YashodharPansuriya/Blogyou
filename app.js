82cc28f6ec10b1a96086f9699ebf538f80ad29f3
YashodharPansuriya <yashodhar.pansuriya@gmail.com>
2023-01-04 21:33:03 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const connectDB = require('./config/db.js');
+
+// Load config
+dotenv.config({ path: './config/config.env' })
+
+const app = express();
+
+//connect db
+connectDB()
+
+//Logging configuration
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
+//Handlebars configuration
+app.engine('.hbs', exphbs({ defaultLayout: 'main' ,extname : '.hbs' }));
+app.set('view engine', '.hbs');
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
