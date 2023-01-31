807fa5619c9a750e2ce26f0c56e8d21523a25180
YashodharPansuriya <yashodhar.pansuriya@gmail.com>
2023-01-31 11:07:24 +0100

    Build model and setup passport google oauth

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
+const passport = require('passport');
+const session = require('express-session');
 const connectDB = require('./config/db.js');
 
 // Load config
 dotenv.config({ path: './config/config.env' })
+
+// Passport configuration
+require('./config/passport')(passport)  
 
 const app = express();
 
@@ -20,6 +25,18 @@ if(process.env.NODE_ENV === 'development') {
 //Handlebars configuration
 app.engine('.hbs', exphbs({ defaultLayout: 'main' ,extname : '.hbs' }));
 app.set('view engine', '.hbs');
+
+//Sessions configuration
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+})
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 //static folder
 app.use(express.static(path.join(__dirname, 'public')));
