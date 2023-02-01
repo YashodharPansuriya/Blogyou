499ce5c4740d831dfddf19e194be7aecdfdd3d55
YashodharPansuriya <yashodhar.pansuriya@gmail.com>
2023-02-01 11:05:59 +0100

    create user routes and store the data in db

--- config/passport.js
+++ config/passport.js
@@ -1,4 +1,4 @@
-const GoogleStrategy = require('passport-google-oauth20');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
 const User = require('../models/User');
 
@@ -6,10 +6,28 @@ module.exports = function (passport) {
     passport.use(new GoogleStrategy({
         clientId: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: 'auth/google/callback'
+        callbackURL: 'http://localhost:3000/auth/google/callback'
     },
         async (accessToken, refreshToken, profile, done) => {
-            console.log(profile)
+            const newUser = {
+                googleId: profile.id,
+                displayName: profile.displayName,
+                firstName: profile.name.givenName,
+                lastName: profile.name.familyName,
+                image: profile.photos[0].value, 
+            }
+
+            try {
+                let user = await User.findOne({ googleId: profile.id})
+                if (user) {
+                    done(null, user)
+                } else {
+                    user = await User.create(newUser)
+                    done(null, user)
+                }
+            } catch (error) {
+                console.error(error);
+            }
         }))
 
     passport.serializeUser((user, done) => {
