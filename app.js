804f1b5f4e1330a699ca5f08945063d6917f676a
YashodharPansuriya <yashodhar.pansuriya@gmail.com>
2023-02-04 10:48:56 +0100

    Build features for view and edit public stories

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 const mongoose = require('mongoose')
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
 const passport = require('passport');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
@@ -20,13 +21,32 @@ const app = express();
 //connect db
 connectDB()
 
+//Body Parser
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+// Method override
+app.use(
+    methodOverride(function (req, res) {
+      if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        // look in urlencoded POST bodies and delete it
+        let method = req.body._method
+        delete req.body._method
+        return method
+      }
+    })
+  )
+
 //Logging configuration
 if(process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
 
+//Hanlerbars Helper
+const { formatDate, stripTags, truncate, editIcon, select } = require('./helpers/hbs')
+
 //Handlebars configuration
-app.engine('.hbs', exphbs({ defaultLayout: 'main' ,extname : '.hbs' }));
+app.engine('.hbs', exphbs({ helpers: { formatDate, stripTags, truncate, editIcon, select } ,defaultLayout: 'main' ,extname : '.hbs' }));
 app.set('view engine', '.hbs');
 
 //Sessions configuration
@@ -41,6 +61,12 @@ app.use(session({
 // Passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
+
+//set global var
+app.use(function(req, res, next) {
+    res.locals.user = req.user || null
+    next()
+})
 
 //static folder
 app.use(express.static(path.join(__dirname, 'public')));
