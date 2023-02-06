91b3839cb1275628239440fd260c67fdc999829c
PansuriyaMaulik <yashodhar.pansuriya@gmail.com>
2023-02-06 10:15:16 +0100

    Delete and search user stories

--- routes/stories.js
+++ routes/stories.js
@@ -114,4 +114,61 @@ router.put('/:id', ensureAuth, async (req, res) => {
   }
 })
 
+  // @desc    Delete story
+  // @route   DELETE /stories/:id
+  router.delete('/:id', ensureAuth, async (req, res) => {
+    try {
+      let story = await Story.findById(req.params.id).lean()
+
+      if (!story) {
+        return res.render('error/404')
+      }
+
+      if (story.user != req.user.id) {
+        res.redirect('/stories')
+      } else {
+        await Story.remove({ _id: req.params.id })
+        res.redirect('/dashboard')
+      }
+    } catch (err) {
+      console.error(err)
+      return res.render('error/500')
+    }
+  })
+
+  // @desc    User stories
+  // @route   GET /stories/user/:userId
+  router.get('/user/:userId', ensureAuth, async (req, res) => {
+    try {
+      const stories = await Story.find({
+        user: req.params.userId,
+        status: 'public',
+      })
+        .populate('user')
+        .lean()
+
+      res.render('stories/index', {
+        stories,
+      })
+    } catch (err) {
+      console.error(err)
+      res.render('error/500')
+    }
+  })
+
+  //@desc Search stories by title
+  //@route GET /stories/search/:query
+  router.get('/search/:query', ensureAuth, async (req, res) => {
+    try{
+        const stories = await Story.find({title: new RegExp(req.query.query,'i'), status: 'public'})
+        .populate('user')
+        .sort({ createdAt: 'desc'})
+        .lean()
+       res.render('stories/index', { stories })
+    } catch(err){
+        console.log(err)
+        res.render('error/404')
+    }
+  }) 
+
 module.exports = router;
